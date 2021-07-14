1f6921737c1d1060b90cf340963ac466e8ec7ea5
stephen <2ndkee@gmail.com>
2021-07-14 15:08:50 -0500

    worked on controllers and repositories

--- queries.sql
+++ queries.sql
@@ -1,7 +1,16 @@
 ## Part 1: Test it with SQL
-
+CREATE TABLE jobs (
+   id INT,
+   name VARCHAR(255),
+   employer VARCHAR(255),
+   location VARCHAR(255),
+   positionType VARCHAR(255),
+   skill VARCHAR(255)
+);
 ## Part 2: Test it with SQL
-
+SELECT *
+FROM employer
+WHERE location = "Saint Louis"
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,37 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(max =50, message = "Please shorten location.")
+    @NotBlank(message = "Please add a location.")
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {         
+    }
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs  = new ArrayList<Job>();
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
